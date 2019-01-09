08dd63f2dce35e3b34ec0713494ea44f10b6638a
Unknown <hstephens333@west-mec.org>
2019-01-09 14:25:57 -0800

    feqfnw
    peepeepoopoo man

---
+++ sync-demo.js
@@ -0,0 +1,13 @@
+/*
+   Exercise-01_01_01
+
+   Filename: sync-demo.js
+   Name: Harrison Stephens
+*/
+
+var fs = require('fs'); // File system package (object name is the same as the variable).
+
+var data = fs.readdirSync('/'); // holds data from the file
+console.log("data: ", data);
+
+console.log("This code is executed last.");
